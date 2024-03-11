3a22e8f948a99ff15d157200b3a541e035515a63
Jacobo <jacobo.palacio@udea.edu.co>
2024-03-11 18:44:42 -0500

    solución de test 1 al 5.

--- reverse.c
+++ reverse.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hola mundo\n");
-    return 0;
+#define MAX_ARGS 3
+int main(int argc, char *argv[]) {
+    //test 1
+    int i;
+    if (argc > MAX_ARGS) {
+        //muchos argumentos
+        fprintf(stderr, "usage: reverse <input> <output>\n");
+        exit(1);
+    }
+    
+    //test2
+    FILE *archivo = fopen(argv[1], "r");
+    if(archivo == NULL){
+        fprintf(stderr, "reverse: cannot open file '/no/such/file.txt'\n");
+        exit(1);
+    }
+
+    //test3
+    // Abriendo el archivo
+    FILE *archivo_salida = fopen(argv[2], "w");
+    // Verificando que el archivo de entrada se pueda abrir
+    if(archivo_salida == NULL){
+        fprintf(stderr, "reverse: cannot open file '/no/such/file.txt'\n");
+        exit(1);
+    }
+
+    //test4
+    if(argv[1] != argv[2]){
+        fprintf(stderr, "reverse: input and output file must differ\n");
+        exit(1);
+    }
+
+    //test5
+    if(argv[1] == argv[2]){
+        fprintf(stderr, "reverse: input and output file must differ\n");
+        exit(1);
+    }
+
+    //test6
+    
+
+    for(i = 0; i < MAX_ARGS; ++i){
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    
+       
 }
-
